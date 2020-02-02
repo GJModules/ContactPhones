c4bb0dd3babc0ad49c9845158e0c7510c1ac7a01
oleg <sad.net79@gmail.com>
2020-02-02 10:17:56 +0200

    start

---
+++ mod_gj_contact_phones.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package    gj_contact_phones
+ *
+ * @author     oleg <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require ModuleHelper::getLayoutPath('mod_gj_contact_phones', $params->get('layout', 'default'));
