1eddbbb04e4c9a7e00ab9e4ad5673117de67405d
oleg <sad.net79@gmail.com>
2020-02-02 13:58:25 +0200

    1.1

--- tmpl/default.php
+++ tmpl/default.php
@@ -13,7 +13,19 @@ defined('_JEXEC') or die;
 // Access to module parameters
 $domain = $params->get('domain', 'https://www.joomla.org');
 ?>
-
+<div class="header-phones">
+    <svg aria-hidden="true" class="header-phones__icon" height="12" width="12">
+        <use xlink:href="#icon-phone" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+    </svg>
+    <!---->
+    <button class="header-phones__button" type="button">
+        <span class="link-dashed">(044) 537-02-22</span>
+        <svg aria-hidden="true" class="header-phones__button-icon" height="7" width="13">
+            <use xlink:href="#icon-chevron-down" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+        </svg>
+    </button>
+    <!---->
+</div>
 <a href="<?php echo $domain; ?>">
 	<?php echo 'gj_contact_phones'; ?>
 </a>
